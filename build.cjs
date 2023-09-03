bec7b460e9ce85280592eaf162c2c517df51fe75
hyc1230 <i@heyc.eu.org>
2023-09-03 12:27:24 +0800

    Initial commit

---
+++ build.cjs
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+const ejs = require("ejs");
+const marked = require("marked");
+
+function build(dir, cfg) {
+    const flist = fs.readdirSync(dir);
+    flist.forEach((file) => {
+        const fullpth = path.join(dir, file);
+        const stat = fs.statSync(fullpth);
+        if (stat.isDirectory()) {
+            build(fullpth, cfg);
+        } else if (path.extname(file) === ".ejs") {
+            const data = fs.readFileSync(fullpth, "utf-8");
+            const content = marked.parse(fs.readFileSync(path.join(dir, path.parse(file).name + ".md"), "utf-8"));
+            cfg.content = content;
+            const html = ejs.render(data, cfg);
+            fs.writeFileSync(path.join(dir, path.parse(file).name + ".html"), html);
+        }
+    });
+}
+
+function stbuild(dir) {
+    const data = fs.readFileSync(path.join(dir, "config.json"), "utf-8");
+    const cfg = JSON.parse(data);
+    build(dir, cfg);
+}
+
+try {
+    stbuild("source");
+    console.log("Build finished");
+} catch(err) {
+    console.error("Build failed: %o", err);
+}
