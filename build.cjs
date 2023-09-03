11f8b19792ea6f2dfd6e3d470d0ef0bc3089d6b8
hyc1230 <i@heyc.eu.org>
2023-09-03 15:45:07 +0800

    fix: 修复未更新的文件（

--- build.cjs
+++ build.cjs
@@ -3,31 +3,40 @@ const path = require("path");
 const ejs = require("ejs");
 const marked = require("marked");
 
-function build(dir, cfg) {
-    const flist = fs.readdirSync(dir);
+function build(srcdir, pubdir, cfg) {
+    const flist = fs.readdirSync(srcdir);
     flist.forEach((file) => {
-        const fullpth = path.join(dir, file);
+        const fullpth = path.join(srcdir, file);
         const stat = fs.statSync(fullpth);
+        const pubfullpth = path.join(pubdir, file);
         if (stat.isDirectory()) {
-            build(fullpth, cfg);
+            if (!fs.existsSync(pubfullpth)) {
+                fs.mkdirSync(pubfullpth);
+            }
+            build(fullpth, pubfullpth, cfg);
         } else if (path.extname(file) === ".ejs") {
             const data = fs.readFileSync(fullpth, "utf-8");
-            const content = marked.parse(fs.readFileSync(path.join(dir, path.parse(file).name + ".md"), "utf-8"));
+            const content = marked.parse(fs.readFileSync(path.join(srcdir, path.parse(file).name + ".md"), "utf-8"));
             cfg.content = content;
             const html = ejs.render(data, cfg);
-            fs.writeFileSync(path.join(dir, path.parse(file).name + ".html"), html, "utf-8");
+            fs.writeFileSync(path.join(pubdir, path.parse(file).name + ".html"), html, "utf-8");
+        } else {
+            fs.copyFileSync(fullpth, pubfullpth);
         }
     });
 }
 
-function stbuild(dir) {
-    const data = fs.readFileSync(path.join(dir, "config.json"), "utf-8");
+function stbuild(srcdir, pubdir) {
+    const data = fs.readFileSync(path.join(srcdir, "config.json"), "utf-8");
     const cfg = JSON.parse(data);
-    build(dir, cfg);
+    if (!fs.existsSync(pubdir)) {
+        fs.mkdirSync(pubdir);
+    }
+    build(srcdir, pubdir, cfg);
 }
 
 try {
-    stbuild("source");
+    stbuild("source", "public");
     console.log("Build finished");
 } catch(err) {
     console.error("Build failed: %o", err);
